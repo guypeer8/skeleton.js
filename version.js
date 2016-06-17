a98c1d40673bea71b6e0ec32b3a2acf7360fd3c4
Guy <guypeer8@gmail.com>
2016-06-17 22:17:05 +0300

    version update on skeleton.js file too

--- version.js
+++ version.js
@@ -1,7 +1,11 @@
 const fs = require('fs');
-const tag = '<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/js-skeleton/(version)/skeleton.js"></script>';
+const script = '<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/js-skeleton/(version)/skeleton.js"></script>';
 const scriptRE = /<script type="text\/javascript" src="https:\/\/cdnjs\.cloudflare\.com\/ajax\/libs\/js-skeleton\/(\d\.\d\.\d)*\/skeleton\.js"><\/script>/gim;
+const title = 'Skeleton JavaScript library v(version)';
+const titleRE = /Skeleton JavaScript library v(\d\.\d\.\d)*/gm;
+const { version } = require('./package.json');
 const files = [
+	'./skeleton.js',
 	'./examples/humanbones/index.html', 
 	'./examples/microblog/index.html',
 	'./examples/TodoMVC/public/index.html',
@@ -9,11 +13,13 @@ const files = [
 ]
 
 const setVersion = () => {
-	const version = JSON.parse(fs.readFileSync('./package.json', 'utf-8')).version;
-	const updatedScript = tag.replace('(version)', version);
+	const updatedScript = script.replace('(version)', version);
+	const updatedTitle = title.replace('(version)', version);
 	files.forEach(file => {
 		let content = fs.readFileSync(file, 'utf-8');
-		let reWrittenContent = content.replace(scriptRE, (str, match) => updatedScript);
+		let reWrittenContent = content
+								.replace(scriptRE, (str, match) => updatedScript)
+								.replace(titleRE, (str, match) => updatedTitle);
 		fs.writeFileSync(file, reWrittenContent, 'utf-8');
 	});
 }
